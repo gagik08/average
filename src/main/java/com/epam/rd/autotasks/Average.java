ae2d155516f4ea9e8dc4221b4af6914aaf2183e8
Gagik Kazandjyan <bi0820a@outlook.com>
2023-05-28 18:47:28 +0400

    Solved at 28/05/2023

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -1,12 +1,26 @@
 package com.epam.rd.autotasks;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Use Scanner methods to read input
-
+        String[] strNums;
+        strNums = scanner.nextLine().split(" ");
+        int average = 0;
+        int count = 0;
+        for (String strNum : strNums) {
+            if (strNum.equals("0")) {
+                break;
+            }
+            count++;
+        }
+        String[] strNums2;
+        strNums2 = Arrays.copyOfRange(strNums, 0, count);
+        for (String s : strNums2) {
+            average += Integer.parseInt(s);
+        }
+        System.out.println(average/count);
     }
-
 }
